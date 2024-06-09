560c747eeb2f0fc06b3b17ff2f86d572a2aeeea4
Denis Denisenko <denis.denisenko@gmail.com>
2024-06-09 23:50:42 +0300

    one hour

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggetIn = { firstName: "Denis", lastName: "DD" };
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggetIn} />
       {children}
     </main>
   );
